23cad7ae0b5353972baa0c5c02c913e0714c7e7a
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-05-12 21:35:21 +0530

    Time: 147 ms (18.79%), Space: 36.1 MB (87.08%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+      int n = nums.size();
+    int count = 0,sum = 0;
+    
+    // create a hashmap
+    unordered_map <int,int> Map;
+    
+    // when no element is chosen, value of sum is 0
+    Map[0] = 1;
+    
+    for (int i = 0; i < n; i++) 
+    {
+        // in each step, find the cumulative sum
+        sum += nums[i];
+        // check if sum-k exists in map 
+        // if exists the increment count by
+        // mapped value for sum-k
+        if (Map.find(sum-k) != Map.end())
+            count += Map[sum-k];
+        
+        // add the cumulative sum obtained until now into the map
+        Map[sum] = Map[sum] + 1;
+    }
+    return count;
+    }
+};
