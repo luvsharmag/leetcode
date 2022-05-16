77509381c227c576dbe7b0d2952741230afab4c0
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-05-16 16:51:39 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (71.81%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(haystack.find(needle) < haystack.length()){
+            return haystack.find(needle);
+        }
+        return -1;
+    }
+};
