8103be9572fc9d2189e90bfa2e7c7e5738c4791b
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-07-14 21:17:58 +0530

    Time: 393 ms (38.71%), Space: 118.1 MB (48.51%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -9,23 +9,75 @@
  * };
  */
 class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        ListNode* curr = head;
-        while(curr != NULL){
-            arr.push_back(curr->val);
-            curr = curr->next;
+private:
+    ListNode* getMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next;
+            if(fast!=NULL) fast = fast->next;
+            slow = slow->next;
         }
-        int s = 0,e = arr.size() - 1;
-        while(s <= e){
-            if(arr[s] == arr[e]){
-                s++;
-                e--;
-            }else{
+        // cout<<slow->val;
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){  
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        while(curr != NULL){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;                        
+        }
+        return prev;
+    }
+    
+public:    
+    bool isPalindrome(ListNode* head) {
+        // vector<int> arr;
+        // ListNode* curr = head;
+        // while(curr != NULL){
+        //     arr.push_back(curr->val);
+        //     curr = curr->next;
+        // }
+        // int s = 0,e = arr.size() - 1;
+        // while(s <= e){
+        //     if(arr[s] == arr[e]){
+        //         s++;
+        //         e--;
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+        // apr2
+        // find size
+        if(head->next == NULL){
+            return true;
+        }
+        // if(headhead->val != head->next->val){
+        //     return false;
+        // }
+        ListNode* middle = getMiddle(head);
+        
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+        
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+        
+        while(head2 != NULL){
+            if(head1->val != head2->val){
                 return false;
             }
+            head1 = head1->next;
+            head2 = head2->next;
         }
+        temp = middle->next;
+        middle->next = reverse(temp);
+        
         return true;
     }
 };
