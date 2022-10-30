170647c64fdf6dab67506450b4f8ac938ce70ac9
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-10-30 18:20:08 +0530

    Added solution - LeetHub

--- Largest Element in Array - GFG/largest-element-in-array.cpp
+++ Largest Element in Array - GFG/largest-element-in-array.cpp
@@ -10,22 +10,31 @@ using namespace std;
 class Solution
 {
 public:
+    int maxele(vector<int> arr,int n,int i){
+        if(i == n-1){
+            return arr[i];
+        }
+        int maxi = maxele(arr,n,i+1);
+        return max(maxi,arr[i]);
+    }
     int largest(vector<int> &arr, int n)
     {
-        //apr
+        //apr1
         // priority_queue<int> pq;
         // for(int i = 0; i < n; i++){
         //     pq.push(arr[i]);
         // }
         // return pq.top();
-        //apr
-        int maxi = arr[0];
-        for(int i = 0; i < n; i++){
-            if(maxi < arr[i]){
-                maxi = arr[i];
-            }
-        }
-        return maxi;
+        //apr2
+        // int maxi = arr[0];
+        // for(int i = 0; i < n; i++){
+        //     if(maxi < arr[i]){
+        //         maxi = arr[i];
+        //     }
+        // }
+        // return maxi;
+        //apr3
+        return maxele(arr,n,0);
     }
 };
 
