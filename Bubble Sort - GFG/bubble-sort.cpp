9c69e2230d5baf9cac18e0201d84a0b688669b56
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-10-31 12:43:32 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -19,18 +19,32 @@ void swap(int *xp, int *yp)
 class Solution
 {
     public:
+    void bsrec(int arr[],int n){
+        if(n == 0 || n == 1){
+            return;
+        }
+        for(int j = 0; j < n - 1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        bsrec(arr,n-1);
+    }
     //Function to sort the array using bubble sort algorithm.
     void bubbleSort(int arr[], int n)
     {
-        for(int i = 0; i < n -1; i++){
-            for(int  j = 0; j < n - i - 1; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
+        // for(int i = 0; i < n -1; i++){
+        //     for(int  j = 0; j < n - i - 1; j++){
+        //         if(arr[j] > arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+        bsrec(arr,n);
     }
 };
 
