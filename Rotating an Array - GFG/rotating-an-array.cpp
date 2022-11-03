12b7a297dcf8df6181988671b29f1f1d6868b8bc
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-11-03 21:03:39 +0530

    Added solution - LeetHub

--- Rotating an Array - GFG/rotating-an-array.cpp
+++ Rotating an Array - GFG/rotating-an-array.cpp
@@ -9,18 +9,29 @@ using namespace std;
 
 class Solution{   
 public:
+    void reverse(int s,int e,int arr[]){
+        while( s < e){
+            swap(arr[s],arr[e]);
+            s++;
+            e--;
+        }
+    }
     void leftRotate(int arr[], int n, int k) {
+        reverse(0,k-1,arr);
+        reverse(k,n-1,arr);
+        reverse(0,n-1,arr);
+        
         //left rotate
-        int temp[k];
-        for(int i = 0 ; i < k ; i++){
-            temp[i] = arr[i];
-        }
-        for(int i = k; i < n; i++){
-            arr[i - k] = arr[i];
-        }
-        for(int i = n- k; i < n; i++){
-            arr[i] = temp[i - n + k];
-        }
+        // int temp[k];
+        // for(int i = 0 ; i < k ; i++){
+        //     temp[i] = arr[i];
+        // }
+        // for(int i = k; i < n; i++){
+        //     arr[i - k] = arr[i];
+        // }
+        // for(int i = n- k; i < n; i++){
+        //     arr[i] = temp[i - n + k];
+        // }
         //right rotate
         // if(n  == 0)return;
         // int temp[k];
