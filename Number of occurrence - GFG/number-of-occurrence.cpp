eb79e2185371e8c7461587e05d5db98d7d6966c2
Luv sharma <83482460+luvsharmag@users.noreply.github.com>
2022-11-17 16:19:37 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int start(int arr[],int n,int x){
+        int s= 0,e = n -1;
+        int ans = -1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            if(arr[mid] > x){
+                e = mid-1;
+            }else if(arr[mid] < x){
+                s = mid+1;
+            }else{
+                ans = mid;
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+    int end(int arr[],int n,int x){
+        int s= 0,e = n -1;
+        int ans = -1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            if(arr[mid] > x){
+                e = mid-1;
+            }else if(arr[mid] < x){
+                s = mid+1;
+            }else{
+                ans = mid;
+                s = mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> find(int arr[], int n , int x )
+    {
+        vector<int> ans;
+        ans.push_back(start(arr,n,x));
+        ans.push_back(end(arr,n,x));
+        return ans;
+    }
+	int count(int arr[], int n, int x) {
+	    vector<int> ans;
+	    ans = find(arr,n,x);
+	    if(ans[1]!= -1 && arr[0] != -1)
+	    return (ans[1] - ans[0]) + 1;
+	    return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
